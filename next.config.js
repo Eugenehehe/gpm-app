a681681b8e321cec69fb52f49b676eef9e781068
Eugenehehe <161422567+Eugenehehe@users.noreply.github.com>
2025-10-28 15:02:46 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
+  // Ensures it builds to a static site for Vercel
   output: 'export',
+
+  // Ignore API routes during static export
+  // (we’ll simulate them as JSON files)
+  trailingSlash: true,
 };
+
 module.exports = nextConfig;
